182cdd2dbb20ad5b5a28cdad60aa23a77a89fbe4
iamrealvinnu <gupta.vinayC@gmail.com>
2025-02-01 01:37:25 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var sampleData = new MLModel1.ModelInput()
+        {
+            Sentence1 = "I love coffee",
+            Sentence2 = "The sky is blue",
+        };
+
+        var result = MLModel1.Predict(sampleData);
+        Console.WriteLine($"Predicted Similarity Score: {result.Score}");
+        Console.ReadLine();
+    }
+}
